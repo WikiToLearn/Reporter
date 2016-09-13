a9753078241c777f5239ffdfbb8a0f754aaf3125
Davide Valsecchi <valsecchi.davide94@gmail.com>
2016-09-13 23:39:45 +0200

    Temporarily saving data on disk instead of in db

--- reporter_app/data_aggregator.py
+++ reporter_app/data_aggregator.py
@@ -1,12 +1,16 @@
 from reporter_app.providers import git_provider as gp
 from reporter_app.providers import phabricator_provider as pp
 from reporter_app.providers import mediawiki_provider as mp
+import json
 
-data = {}
+
+datas = json.load(open("data_json","r"))
 
 
-def fetch_data(start_date, end_date, repos, phab_groups, mediawiki_langs ):
-    if "git" not in data:
+def fetch_data(id, start_date, end_date, repos, phab_groups, mediawiki_langs ):
+    if id not in datas:
+        data = {}
+        datas[id] = data
         data["git"] = []
         #getting data from git
         print("Fetching git data...")
@@ -17,11 +21,26 @@ def fetch_data(start_date, end_date, repos, phab_groups, mediawiki_langs ):
                     "stats_per_user" : gp.get_contributes_per_user(repo, start_date, end_date),
                     "stats_total": gp.get_total_contributions(repo, start_date, end_date)
                  })
-    #getting data from phab
-    if "phabricator" not in data:
+        #getting data from phab
         data["phabricator"] = {}
         print("Fetching phabricator data...")
         for ph_gr in phab_groups:
             ph_data = pp.calculate_generic_stats(phab_groups[ph_gr], start_date, end_date)
             data["phabricator"][ph_gr] = ph_data
-    return data
+    json.dump(datas,open("data_json","w"), indent=4)
+    return datas[id]
+
+
+def calculate_totals_phabricator(id):
+    phdata = datas[id]["phabricator"]
+    result = {
+                "opened" : 0,
+                "closed" : 0,
+                "comments" : 0,
+                "total_open" : 0}
+    for pr in phdata.values():
+        result["opened"] += pr["opened"]
+        result["closed"] += pr["closed"]
+        result["comments"] += pr["comments"]
+        result["total_open"] += pr["total_open"]
+    return result
--- reporter_app/reporter_ui.py
+++ reporter_app/reporter_ui.py
@@ -1,21 +1,22 @@
 from reporter_app import app
 from flask import render_template, jsonify, request
 import datetime
-import reporter_app.data_aggregator as dg
+import reporter_app.data_aggregator as dagg
 
 
 @app.route('/manager', methods=['GET'])
 def manager():
     return render_template('create_report.template')
 
-@app.route('/report/<date>', methods=['GET'])
-def report(date):
-    start_date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
-    end_date = datetime.date.today()
+@app.route('/report/<id>', methods=['GET'])
+def report(id):
+    #getting data for the dates
+    data = dagg.datas[id]
+    return render_template('report.template',
+                           ph_projects = data["phabricator"],
+                           total_ph = dagg.calculate_totals_phabricator(id))
 
-    return render_template('report.template', ph_projects = dg.data["phabricator"])
 
-report_ready = False
 
 @app.route('/report/generate', methods=['POST'])
 def generate_report():
@@ -27,7 +28,10 @@ def generate_report():
     for p in query["ph_projs"]:
         ph_projs[p] = []
         for id in query["ph_projs"][p]:
-            ph_projs[p].append(dg.pp.lookup_project_phabid_byid(id))
+            ph_projs[p].append(dagg.pp.lookup_project_phabid_byid(id))
+    id = generate_id(start_date, end_date)
+    dagg.fetch_data(id, start_date, end_date,repos, ph_projs, [])
+    return jsonify(dagg.datas[id])
 
-    dg.fetch_data(start_date, end_date,repos, ph_projs, [])
-    return jsonify(dg.data)
+def generate_id(start_date, end_date):
+    return start_date.strftime("%Y-%m-%d")+"_"+end_date.strftime("%Y-%m-%d")
