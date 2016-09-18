eff89d8f5cf0b2f61aa541d4c0162a65b7356a4b
Davide Valsecchi <valsecchi.davide94@gmail.com>
2016-09-18 18:17:24 +0200

    Little graphical fixes
    Signed-off-by: Davide Valsecchi <valsecchi.davide94@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.5
 
 RUN pip3 install requests==2.11.1
-RUN pip3 install flask=0.11
+RUN pip3 install flask==0.11
 
 COPY . /app
 WORKDIR /app
--- reporter_app/static/js/index.js
+++ reporter_app/static/js/index.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     $('#phab_general_tabel').dataTable({
-        "order" : [[4, "desc"]]
+        "order" : [[1, "desc"]]
     });
     $('#phab_users_table').dataTable({
-        "order" : [[2, "desc"]]
+        "order" : [[1, "desc"]]
     });
     $('#git_projs_table').dataTable({
         "order" : [[1, "desc"]]
